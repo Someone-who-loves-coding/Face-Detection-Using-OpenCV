e1d4b921f398b35d0470d93f24951000b193d5e1
Someone-who-loves-coding <ps2284@srmist.edu.in>
2023-10-04 23:23:32 +0530

    Face Detection complete

--- Face_Detection.py
+++ Face_Detection.py
@@ -1,10 +1,22 @@
 import cv2
 
-a = cv2.VideoCapture(0)
+video_cap = cv2.VideoCapture(0)
 
-while True :
-    ret, c= a.read()
-    cv2.imshow("Live Vid", c)
-    if cv2.waitKey(10) == ord("a") :
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+while True:
+    ret, frame = video_cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.3,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    cv2.imshow("Live Video", frame)
+    if cv2.waitKey(10) == ord("a"):
         break
-c.release()
+video_cap.release()
